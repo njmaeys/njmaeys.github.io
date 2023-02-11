70d72d9bcd1166868427bcf53f3642cd31223daa
Nate Maeys <natemaeys@nates-mbp.lan>
2023-02-11 12:30:03 -0600

    bit more style

--- listings.js
+++ listings.js
@@ -10,21 +10,24 @@ fetch('listings.json')
         
             let image = document.createElement("img");
             image.src = item.image;
-            childDiv.appendChild(image);
         
             let title = document.createElement("h3");
             title.innerHTML = item.title;
-            childDiv.appendChild(title);
         
             let button = document.createElement("button");
             button.innerHTML = "Purchase $" + item.price;
             button.onclick = function() {
                 window.open(item.url, "_blank");
             };
+
+            childDiv.appendChild(image);
+            childDiv.appendChild(title);
             childDiv.appendChild(button);
 
             childDiv.className = "image fit";
-        
+            childDiv.style.borderBottom = "1px solid lightgrey";
+            childDiv.style.paddingBottom = "10px";
+
             parentDiv.appendChild(childDiv);
         });
     });
